9d8178f71be067d5a44c29f5d94bd38a112cc570
Andrej Nano <nano@andrejnano.sk>
2017-10-14 16:49:23 +0200

    Add basic HTML/CSS/JS structure
    Added some basic html structure with optimized css styling for
    different browsers, set defaults, basic styling, imported Font-Awesome
    font for icons..

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*
+    JS script for a todo-list app project
+    made as a school project @ FIT VUT, BRNO
+
+    + Projekt ITU 2017 +
+
+    Authors:
+    ----------------------------------------------
+    Gabzdyl Dominik	    xgabzd00
+    Nano Andrej	        xnanoa00     {veduci}
+    Sabo Jozef	        xsaboj00
+
+*/
+
+console.log("Hello World :)");
