182972ee1570c4d42b8583993229aadfabb96a30
Werewolf007 <drgordonfreeman@seznam.cz>
2017-12-01 02:22:04 +0100

    Make input forms function

--- js/main.js
+++ js/main.js
@@ -3,3 +3,22 @@
 MAIN JS FILE
 -----------------
 */
+
+input_forms = [ 'input-form-ifj', 'input-form-ial', 'input-form-itu',
+				'input-form-inm', 'input-form-inp', 'input-form-ips',
+				'input-form-izp', 'input-form-isu']
+
+
+function highlight(object_id)
+{
+	/* Firstly, reset brightness to default values */
+	for (var i=0; i<input_forms.length; i++)
+	{
+		document.getElementById(input_forms[i]).style["-webkit-filter"] = "brightness(100%)";
+	}
+
+	/* Then highlight the selected input form cell */
+	var cell = document.getElementById(object_id);
+	cell.style["-webkit-filter"] = "brightness(125%)";
+}
+
